6a979d13c7854c6ed12da40e561a26d4e75ebfdc
swati.irlapalle <Swati@123>
2020-10-29 10:01:24 +0530

    un unsed import removed

--- src/main/java/com/anz/account/service/AccountService.java
+++ src/main/java/com/anz/account/service/AccountService.java
@@ -1,11 +1,8 @@
 package com.anz.account.service;
 
-import com.anz.account.entity.Account;
-import com.anz.account.entity.Transaction;
 import com.anz.account.response.AccountResponse;
 import com.anz.account.response.TransactionResponse;
 
-import java.util.List;
 import java.util.Set;
 
 public interface AccountService {
